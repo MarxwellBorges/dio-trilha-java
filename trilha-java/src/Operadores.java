fc9c66a453459ade104977eed331c63744eb6241
Marxwell Borges <108357112+MarxwellBorges@users.noreply.github.com>
2023-03-23 11:32:02 -0300

    testando

---
+++ trilha-java/src/Operadores.java
@@ -0,0 +1,12 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String  nomeCompleto = "Linguagem" + "Java";
+         System.out.println(nomeCompleto);
+    }
+   
+    
+    
+    
+    }
+
+}
